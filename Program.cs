c636bb0689c85432e41cd0faa7c253f5cabf9687
Yi Fu Ji <yj952830@wtstudents.wtamu.edu>
2018-09-10 12:01:28 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterestThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
